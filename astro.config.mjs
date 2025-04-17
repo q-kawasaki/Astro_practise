ac1bd3f44228687b74658a5075fe42e5550c6785
Masayuki Kawasaki <kawasaki@q-co.jp>
2025-04-17 15:51:57 +0900

    チュートリアル 終了

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://q-kawasaki-astro-practice.netlify.app/",
+  integrations: [preact()]
+});
